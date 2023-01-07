67ab59b8de7e8d00d98ef8760d73845802b54692
JUST1TURKI <Tsgtsg2002@gmail.com>
2023-01-08 02:29:18 +0300

    Changes

---
+++ script.js
@@ -0,0 +1,107 @@
+
+
+document.querySelector(".menu-btn").addEventListener("click",()=>document.querySelector(".main-menu").classList.toggle("show"));
+
+// todo_____________________________________________________________________________
+
+let isShow = true;
+
+const course = document.querySelector('.course-slide');
+function toggleShowContent(){
+    if(isShow){
+        course.style.display='block';
+        isShow = false;
+    }else{
+        course.style.display='none';
+        isShow = true;
+    }
+    
+}
+const note = document.querySelector('.course-note');
+function toggleShowNote(){
+    if(isShow){
+        note.style.display='block';
+        isShow = false;
+    }else{
+        note.style.display='none';
+        isShow = true;
+    }
+    
+}
+const old = document.querySelector('.course-old');
+function toggleShowOld(){
+    if(isShow){
+        old.style.display='block';
+        isShow = false;
+    }else{
+        old.style.display='none';
+        isShow = true;
+    }
+    
+}
+const q = document.querySelector('.course-q');
+function toggleShowQ(){
+    if(isShow){
+        q.style.display='block';
+        isShow = false;
+    }else{
+        q.style.display='none';
+        isShow = true;
+    }
+    
+}
+const vid = document.querySelector('.course-vid');
+function toggleShowVid(){
+    if(isShow){
+        vid.style.display='block';
+        isShow = false;
+    }else{
+        vid.style.display='none';
+        isShow = true;
+    }
+    
+}
+const pro = document.querySelector('.course-pro');
+function toggleShowPro(){
+    if(isShow){
+        pro.style.display='block';
+        isShow = false;
+    }else{
+        pro.style.display='none';
+        isShow = true;
+    }
+    
+}
+const hws = document.querySelector('.course-hws');
+function toggleShowHws(){
+    if(isShow){
+        hws.style.display='block';
+        isShow = false;
+    }else{
+        hws.style.display='none';
+        isShow = true;
+    }
+    
+}
+const lab = document.querySelector('.course-lab');
+function toggleShowLab(){
+    if(isShow){
+        lab.style.display='block';
+        isShow = false;
+    }else{
+        lab.style.display='none';
+        isShow = true;
+    }
+    
+}
+
+
+
+
+function change( el )
+{
+    if ( el.value === "show" )
+        el.value = "Hide";
+    else
+        el.value = "show";
+}
